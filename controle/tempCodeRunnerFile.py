387b15980b060bab47bad1c6f911b2f330723033
herloncosta <herlon36@gmail.com>
2024-03-13 14:46:19 -0300

    updated

--- controle/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-ano_nascimento = int(input("Qual o seu ano de nascimento: "))
-
-# if (ano_nascimento > geracoes["Silenciosa"][0] and ano_nascimento < geracoes["Silenciosa"][1]):
-#     print("Geração: Silenciosa")
