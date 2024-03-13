efbb2b0cbe10301be1bb8fe66f711d55d76b46de
herloncosta <herlon36@gmail.com>
2024-03-13 14:45:37 -0300

    updated

---
+++ controle/condicionais-1.py
@@ -0,0 +1,8 @@
+a = 10
+
+if a > 5:
+    print("O valor é maior do que 5!")
+else:
+    print("O valor não é maior do que 5!")
+
+print("Fim do código!")
---
+++ controle/condicionais-2.py
@@ -0,0 +1,15 @@
+print(range(1928, 1945))
+
+geracoes = {
+    "silenciosa": [1928, 1945],
+    "BabyBoomers": [1946, 1964],
+    "X": [1965, 1980],
+    "Millennials": [1981, 1996],
+    "Centennials": [1997, 2012],
+    "Alpha": [2013, 2024]
+}
+
+# ano_nascimento = int(input("Qual o seu ano de nascimento: "))
+
+# if (ano_nascimento > geracoes["Silenciosa"][0] and ano_nascimento < geracoes["Silenciosa"][1]):
+#     print("Geração: Silenciosa")
