eaf22c2300928e8a48b2c440b7915e439eef5bf0
herloncosta <herlon36@gmail.com>
2024-03-13 22:14:52 -0300

    updated

--- controle/condicionais-2.py
+++ controle/condicionais-2.py
@@ -1,15 +1,23 @@
-print(range(1928, 1945))
-
 geracoes = {
-    "silenciosa": [1928, 1945],
-    "BabyBoomers": [1946, 1964],
-    "X": [1965, 1980],
-    "Millennials": [1981, 1996],
-    "Centennials": [1997, 2012],
-    "Alpha": [2013, 2024]
+    "silenciosa": 1945,
+    "baby_boomers": 1964,
+    "x": 1980,
+    "millennials": 1996,
+    "centennials": 2012,
+    "alpha": 2024
 }
 
-# ano_nascimento = int(input("Qual o seu ano de nascimento: "))
+ano_nascimento = int(input("Qual o seu ano de nascimento: "))
 
-# if (ano_nascimento > geracoes["Silenciosa"][0] and ano_nascimento < geracoes["Silenciosa"][1]):
-#     print("Geração: Silenciosa")
+if (ano_nascimento <= geracoes["silenciosa"]):
+    print(f"Sua geração é: Silenciosa")
+elif (ano_nascimento <= geracoes["baby_boomers"]):
+    print(f"Sua geração é: Baby Boomer")
+elif (ano_nascimento <= geracoes["x"]):
+    print(f"Sua geração é: X")
+elif (ano_nascimento <= geracoes["millennials"]):
+    print(f"Sua geração é: Millennials")
+elif (ano_nascimento <= geracoes["centennials"]):
+    print(f"Sua geração é: Centennials")
+elif (ano_nascimento <= geracoes["alpha"]):
+    print(f"Sua geração é: Alpha")
